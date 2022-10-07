8670f781beceeb7ada791bd10893a2763c73391b
nateosborne <natedresdeno@gmail.com>
2022-10-07 00:59:54 -0400

    homepage

--- app/model.js
+++ app/model.js
@@ -1,6 +1,64 @@
 // This is my model and it handles on the service calls
-var homeContent = `<h1>home</h1>`
-var aboutContent = `<h1>about</h1>`
+var homeContent = `<div class="homepage">
+<div class="homehero">
+  <div class="homeherotext">
+    <p class="homeherotexttop">A PLACE IS <br />IS NOT A PLACE</p>
+    <p>UNTIL IT HAS A BOOKSTORE</p>
+  </div>
+</div>
+</div>
+<div class="homepage2">
+<div class="homebookscont">
+  <div class="book1cont">
+    <div class="book1img"></div>
+    <div class="book1text">
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
+        tortor condimentum lacinia quis vel eros.
+      </p>
+    </div>
+    <div class="book1price">
+      <p>$15.99</p>
+    </div>
+    <button class="addtocart">ADD TO CART</button>
+  </div>
+
+  <div class="book1cont">
+    <div class="book2img"></div>
+    <div class="book1text">
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
+        tortor condimentum lacinia quis vel eros.
+      </p>
+    </div>
+    <div class="book1price">
+      <p>$25.99</p>
+    </div>
+    <button class="addtocart">ADD TO CART</button>
+  </div>
+
+  <div class="book1cont">
+    <div class="book3img"></div>
+    <div class="book1text">
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
+        tortor condimentum lacinia quis vel eros.
+      </p>
+    </div>
+    <div class="book1price">
+      <p>$15.99</p>
+    </div>
+    <button class="addtocart">ADD TO CART</button>
+  </div>
+</div>
+</div>`
+var aboutContent = ``
 
 function setCurrentPageContent(pageID) {
     let contentName = pageID + "Content";
