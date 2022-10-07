b0c779d48b248f543faecef92e7a29cbcea7456a
Braun, Sierra <siebraun@iu.edu>
2022-10-07 12:23:53 -0400

    added books page, switched to different mvc model

--- app/app.js
+++ app/app.js
@@ -1,16 +1,95 @@
+// import * as MODEL from "./model.js";
+
+// function initListeners() {
+//   $("nav a").click((e) => {
+//     e.preventDefault();
+//     let btnID = e.currentTarget.id;
+
+//     MODEL.setCurrentPageContent(btnID);
+//   });
+// }
+
+// $(document).ready(function () {
+//   // MODEL.setCurrentPageContent("home");
+//   initListeners();
+// });
+
 import * as MODEL from "./model.js";
 
-function initListeners(){
+function route() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  let pageIDarray = pageID.split("/");
+  pageID = pageIDarray[0];
+  let subPageID = pageIDarray[1];
 
-    $("nav a").click((e) => {
-        e.preventDefault();
-        let btnID = e.currentTarget.id;
-        
-        MODEL.setCurrentPageContent(btnID);
-    });
+  console.log("hash " + pageID);
+  //   MODEL.changePage(pageID);
+
+  if (pageID == "") {
+    MODEL.changePage("home");
+  } else {
+    if (pageID == subPageID) {
+      MODEL.changePage(pageID);
+    } else {
+      MODEL.changePage(pageID, subPageID);
+    }
+  }
+}
+
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  //   console.log(hashTag + ' ' + pageID);
+  if (pageID == "" || pageID == "home") {
+    MODEL.changePage(pageID, initSubmitListener);
+  } else if (pageID == "books") {
+    MODEL.changePage(pageID, buyNow);
+  } else {
+    MODEL.changePage(pageID);
+  }
+}
+
+function initURLListener() {
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+}
+
+function buyNow() {
+  $(".book-info button").on("click", function (e) {
+    let bookID = e.currentTarget.id;
+    console.log(bookID);
+    MODEL.addToCart(bookID);
+  });
+}
+
+function initSubmitListener() {
+  $("#submit").on("click", function (e) {
+    // console.log("submit").val();
+    let fn = $("#fname").val();
+    let ln = $("#lname").val();
+    let em = $("#email").val();
+    let pw = $("#pw").val();
+    if (fname === "") {
+      alert("enter your first name");
+    } else if (lname == "") {
+      alert("enter your last name");
+    } else if (email == "") {
+      alert("enter your email");
+    } else if (pw == "") {
+      alert("enter an email");
+    } else {
+      let userObj = {
+        firstName: fn,
+        lastName: ln,
+        email: em,
+      };
+
+      MODEL.setUserInfo(userObj);
+    }
+  });
 }
 
 $(document).ready(function () {
-    MODEL.setCurrentPageContent("home");
-    initListeners();
-})
+  initURLListener();
+});
--- app/model.js
+++ app/model.js
@@ -1,63 +1,63 @@
 // This is my model and it handles on the service calls
-var homeContent = `<div class="homepage">
-<div class="homehero">
-  <div class="homeherotext">
-    <p class="homeherotexttop">A PLACE IS <br />IS NOT A PLACE</p>
-    <p>UNTIL IT HAS A BOOKSTORE</p>
-  </div>
-</div>
-</div>
-<div class="homepage2">
-<div class="homebookscont">
-  <div class="book1cont">
-    <div class="book1img"></div>
-    <div class="book1text">
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
-        tortor condimentum lacinia quis vel eros.
-      </p>
-    </div>
-    <div class="book1price">
-      <p>$15.99</p>
-    </div>
-    <button class="addtocart">ADD TO CART</button>
-  </div>
+// var homeContent = `<div class="homepage">
+// <div class="homehero">
+//   <div class="homeherotext">
+//     <p class="homeherotexttop">A PLACE IS <br />IS NOT A PLACE</p>
+//     <p>UNTIL IT HAS A BOOKSTORE</p>
+//   </div>
+// </div>
+// </div>
+// <div class="homepage2">
+// <div class="homebookscont">
+//   <div class="book1cont">
+//     <div class="book1img"></div>
+//     <div class="book1text">
+//       <p>
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//         eiusmod tempor incididunt ut labore et dolore magna aliqua."
+//         Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
+//         tortor condimentum lacinia quis vel eros.
+//       </p>
+//     </div>
+//     <div class="book1price">
+//       <p>$15.99</p>
+//     </div>
+//     <button class="addtocart">ADD TO CART</button>
+//   </div>
 
-  <div class="book1cont">
-    <div class="book2img"></div>
-    <div class="book1text">
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
-        tortor condimentum lacinia quis vel eros.
-      </p>
-    </div>
-    <div class="book1price">
-      <p>$25.99</p>
-    </div>
-    <button class="addtocart">ADD TO CART</button>
-  </div>
+//   <div class="book1cont">
+//     <div class="book2img"></div>
+//     <div class="book1text">
+//       <p>
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//         eiusmod tempor incididunt ut labore et dolore magna aliqua."
+//         Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
+//         tortor condimentum lacinia quis vel eros.
+//       </p>
+//     </div>
+//     <div class="book1price">
+//       <p>$25.99</p>
+//     </div>
+//     <button class="addtocart">ADD TO CART</button>
+//   </div>
 
-  <div class="book1cont">
-    <div class="book3img"></div>
-    <div class="book1text">
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
-        tortor condimentum lacinia quis vel eros.
-      </p>
-    </div>
-    <div class="book1price">
-      <p>$15.99</p>
-    </div>
-    <button class="addtocart">ADD TO CART</button>
-  </div>
-</div>
-</div>`
+//   <div class="book1cont">
+//     <div class="book3img"></div>
+//     <div class="book1text">
+//       <p>
+//         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//         eiusmod tempor incididunt ut labore et dolore magna aliqua."
+//         Urna nunc id cursus metus aliquam eleifend mi. Massa vitae
+//         tortor condimentum lacinia quis vel eros.
+//       </p>
+//     </div>
+//     <div class="book1price">
+//       <p>$15.99</p>
+//     </div>
+//     <button class="addtocart">ADD TO CART</button>
+//   </div>
+// </div>
+// </div>`;
 var aboutContent = `<div class="aboutpage">
 <div class="aboutcont">
   <div class="about1">
@@ -109,15 +109,134 @@ var aboutContent = `<div class="aboutpage">
     </div>
   </div>
 </div>
-</div>`
+</div>`;
 
-function setCurrentPageContent(pageID) {
-    let contentName = pageID + "Content";
-    $("#app").html(eval(contentName));
+// function setCurrentPageContent(pageID) {
+//     let contentName = pageID + "Content";
+//     $("#app").html(eval(contentName));
+// }
+
+// function getLastName(lastName){
+//     console.log();
+// }
+
+// export { setCurrentPageContent };
+
+var userInfo = {};
+var cart = [];
+var bookList = [
+  {
+    image: "twilight-box-set.jpg",
+    price: "99.99",
+  },
+
+  {
+    image: "hp-box-set.jpg",
+    price: "100",
+  },
+
+  {
+    image: "got-box-set.jpg",
+    price: "100",
+  },
+];
+
+var bookList2 = [
+  {
+    image: "finding-me.jpg",
+    price: "27.99",
+  },
+
+  {
+    image: "mlk-biography.jpg",
+    price: "19.99",
+  },
+
+  {
+    image: "elenor-roosevelt-biography.jpg",
+    price: "17.99",
+  },
+];
+
+export function changePage(pageID, callback) {
+  if (pageID == "" || pageID == "home") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      // console.log("data " + data);
+      $("#app").html(data);
+      callback();
+    });
+  } else if (pageID == "books") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      // console.log("data " + data);
+      $("#app").html(data);
+      $.each(bookList, function (idx, book) {
+        $(".book-row").append(`<div class="book-row--book">
+        <div class="image">
+          <img src="images/${book.image}" alt="" />
+        </div>
+        <div class="desc">
+        
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et
+          malesuada fames ac.</p>
+          <p><b>$${book.price}</b></p>
+  
+          <button id="${idx}">Buy</button>
+        </div>
+      </div>`);
+      });
+
+      // book list 2
+      $.each(bookList2, function (idx, book) {
+        $(".book-row2").append(`<div class="book-row--book">
+        <div class="image">
+          <img src="images/${book.image}" alt="" />
+        </div>
+        <div class="desc">
+        
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et
+          malesuada fames ac.</p>
+          <p><b>$${book.price}</b></p>
+  
+          <button id="${idx}">Buy</button>
+        </div>
+      </div>`);
+      });
+      callback();
+    });
+  } else {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+      $.each(cart, function (idx, cartItem) {
+        console.log(bookList[cartItem]);
+
+        let book = bookList[cartItem];
+
+        $("items").append(`<div class="book">
+        <div class="book-img">
+          <img src="assets/${book.bookImg}" alt="" />
+        </div>
+        <div class="book-info">
+          <h4>${book.bookTitle}</h4>
+          <p>Author: ${book.bookAuthor}</p>
+          <p>Price: $${book.price}</p>
+  
+          <button id="${idx}">Buy</button>
+        </div>
+      </div>`);
+      });
+    });
+  }
 }
 
-function getLastName(lastName){
-    console.log();
+export function setUserInfo(userObject) {
+  userInfo = userObject;
+  console.log(userInfo);
 }
 
-export { setCurrentPageContent };
+export function addToCart(bookIdx) {
+  cart.push(bookIdx);
+  $("#cartCount").html(cart.length.toString());
+}
